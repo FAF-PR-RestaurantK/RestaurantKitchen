367069e63b9e033464392f465278ff94dd1dbfc8
Volcov Oleg <bojikch15@gmail.com>
2021-09-27 01:08:10 +0300

    Added possibility in `main` run server with `Cooks` and `OrderManager`.

--- src/main.go
+++ src/main.go
@@ -3,25 +3,50 @@ package main
 import (
 	"encoding/json"
 	"github.com/FAF-PR-RestaurantK/RestaurantKitchen/src/configuration"
+	"github.com/FAF-PR-RestaurantK/RestaurantKitchen/src/cook"
 	"github.com/FAF-PR-RestaurantK/RestaurantKitchen/src/item"
+	"github.com/FAF-PR-RestaurantK/RestaurantKitchen/src/orderManager"
 	"github.com/FAF-PR-RestaurantK/RestaurantKitchen/src/orderRout"
 	"github.com/FAF-PR-RestaurantK/RestaurantKitchen/src/singleton"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 const (
 	ConfPath  = "./conf/configuration.json"
 	ItemsPath = "./conf/items.json"
+	CooksPath = "./conf/cooks.json"
 )
+
+var CONF *configuration.Configuration
 
 func main() {
 	conf := GetConf()
 	container := GetItemContainer()
+	cooks := GetCooks()
 
 	singleton.Singleton().Set("items", container)
+	singleton.Singleton().Set("conf", &conf)
+
+	configuration.TimeUnit = time.Duration(1)
+
+	for i := range cooks {
+		cooks[i].Id = i
+		cooks[i].SetThreads(cooks[i].Proficiency)
+	}
+
+	orderManager.SetItems(container.GetList())
+	orderManager.SetConf(&conf)
+	orderManager.SetCooks(cooks)
+
+	go orderManager.Get().Run()
+
+	for i := range cooks {
+		go cooks[i].Run()
+	}
 
 	http.HandleFunc(conf.OrderRout, orderRout.OrderHandler)
 
@@ -75,4 +100,27 @@ func GetItemContainer() *item.Container {
 	}
 
 	return item.NewContainer(itemList)
+}
+
+func GetCooks() []*cook.Cook {
+	var cooks []*cook.Cook
+
+	cooksFile, _ := os.Open(CooksPath)
+	defer func(itemListFile *os.File) {
+		_ = itemListFile.Close()
+	}(cooksFile)
+
+	jsonData, err := io.ReadAll(cooksFile)
+	if err != nil {
+		log.Fatalf("exit: %s\n", err.Error())
+		return nil
+	}
+
+	err = json.Unmarshal(jsonData, &cooks)
+	if err != nil {
+		log.Fatalf("exit: %s\n", err.Error())
+		return nil
+	}
+
+	return cooks
 }
