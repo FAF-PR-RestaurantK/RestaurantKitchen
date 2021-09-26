375c43e64ff4c333794b77083e8a9fbd112c8959
Volcov Oleg <bojikch15@gmail.com>
2021-09-27 01:00:11 +0300

    Added new method for `Item` array sharing in `Container` struct.

--- src/item/Container.go
+++ src/item/Container.go
@@ -8,11 +8,15 @@ func NewContainer(items []Item) *Container {
 	return &Container{items: items}
 }
 
-func (container *Container) Get(id uint8) (*Item, bool) {
+func (container *Container) Get(id int) (*Item, bool) {
 	for i := range container.items {
 		if container.items[i].Id == id {
 			return &container.items[i], true
 		}
 	}
 	return nil, false
+}
+
+func (container *Container) GetList() []Item {
+	return container.items
 }
